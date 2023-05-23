8f695e2761cce64cce0b5f9271bc33e041f79cd3
yudhabahrul <yudhabahrul40@gmail.com>
2023-05-23 12:52:46 +0700

    membuat halaman register

---
+++ finbbyapp/app/src/main/java/com/example/finbbyapp/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finbbyapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finbbyapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
