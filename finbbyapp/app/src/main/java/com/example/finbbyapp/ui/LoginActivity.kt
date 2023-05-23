ebd7df7278a8cafd37bfca06c25d0d2a2e1d3ad7
yudhabahrul <yudhabahrul40@gmail.com>
2023-05-23 14:36:35 +0700

    membuat halaman home

--- finbbyapp/app/src/main/java/com/example/finbbyapp/ui/LoginActivity.kt
+++ finbbyapp/app/src/main/java/com/example/finbbyapp/ui/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.finbbyapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finbbyapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
